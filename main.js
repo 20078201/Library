a835a05dfbc202d002c02be606fb5ed83baf9cf9
Dat Thinkpad <dat-ngo@hotmail.com>
2022-07-17 16:43:37 +0800

    Have added the ability to remove individual book that are displayed on screen. Have also added null checks

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let myLibrary = []
+const bookTable = document.querySelector('#books')
+const addBookBtn = document.querySelector('[data-book="add"]')
 
 const Book = (title, author, pages, isRead) => {
 
@@ -13,28 +15,80 @@ function addBookToLibrary(book) {
     myLibrary.push(book)
 }
 
-const bookTable = document.querySelector('#books')
-const addBookBtn = document.querySelector('[data-book="add"]')
+function remove(array, ...inputs) {
+    return array.filter(v => {
+        return !inputs.includes(v)
+    })
+}
+
+function update() {
+    // Clear the screen
+    while (bookTable.firstChild) {
+        bookTable.removeChild(bookTable.firstChild)
+    }
+    
+    // remake the cards and display it on the screen
+    myLibrary.forEach(book => {
+        const newCard = createBookDiv(book)
+        bookTable.append(newCard)
+    })
+}
 
 function createBookDiv(bookObj) {
     const container = document.createElement('div')
     container.classList.add('card')
+
+    const cardHeader = document.createElement('div')
+    cardHeader.classList.add('card-header')
+
     const title = document.createElement('h2')
     title.textContent = bookObj.title
+
     const author = document.createElement('p')
     author.textContent = bookObj.author
+
     const bookLength = document.createElement('p')
     bookLength.textContent = bookObj.pages
+
+    cardHeader.append(title)
+    cardHeader.append(author)
+    cardHeader.append(bookLength)
+
+    // hasRead
     const hasRead = document.createElement('button')
-    if(bookObj.isRead){
-        hasRead.textContent = "book has been read"
-    } else if (!bookObj.isRead) {
-        hasRead.textContent = "book has not been finished"
+    hasRead.classList.add('card-button')
+    if (bookObj.isRead === 'true'){
+        hasRead.textContent = "Read"
+        hasRead.setAttribute('data-read', 'true')
+    } else if (bookObj.isRead === 'false') {
+        hasRead.textContent = "Not Read"
+        hasRead.setAttribute('data-read', 'false')
     }
+    
+    hasRead.addEventListener('click', () => {
+        const read = hasRead.getAttribute('data-read')
+        if (read === 'true') {
+            hasRead.setAttribute("data-read", false)
+        } else if (read === 'false') {
+            hasRead.setAttribute("data-read", true)
+        }
+    })
+
+    
     const removeBook = document.createElement('button')
     removeBook.textContent = 'REMOVE'
+    removeBook.classList.add('card-button')
+    removeBook.classList.add('remove-book')
 
-    container.append(title, author, bookLength, hasRead, removeBook)
+    removeBook.addEventListener('click', () => {
+        // Remove element from array
+        myLibrary = remove(myLibrary, bookObj)
+        // update
+        update()
+    })
+
+    container.append(cardHeader, hasRead, removeBook)
+
 
     return container
 }
@@ -43,20 +97,36 @@ addBookBtn.addEventListener('click', () => {
     const title = document.querySelector('#title')
     const author = document.querySelector('#author')
     const pages = document.querySelector('#pages')
-    const isRead = document.querySelectorAll('input[type="radio"]')
-    if (isRead.value === 'true' && isRead.checked) {
-        isRead.textContent = 'This book has been read'
-    } else if (isRead.value = 'false' && isRead.checked) {
-        isRead.textContent = 'This book has not been read yet'
+    const allRadioButtons = document.querySelectorAll('input[name="isRead"]')
+    let activeRadioButton;
+
+    for (let i = 0; i < allRadioButtons.length; i++) {
+        if (allRadioButtons[i].checked === false) continue
+        activeRadioButton = allRadioButtons[i];
+    }   
+  
+    if (title.value === "" || author.value === "" || pages.value === "") {
+            alert("You need to fill the form")
+            return
+    } 
+
+    if (pages.value <= 0) {
+        alert("The page number can't be less than 0")
+        return
     }
 
-    const userBook = createBookDiv(Book(title.textContent, author.textContent, pages.textContent, 
-        isRead.textContent)) 
-    
-    myLibrary.push(userBook)
-    bookTable.append(userBook)
-})
 
+    // Create new book object
+    const newBook = Book(title.value, author.value, pages.value, activeRadioButton.value)
+    myLibrary.push(newBook)
+
+    // Reset the inputs
+    title.value = ''
+    author.value = ''
+    pages.value = ''
+
+    update()
+})
 
 
 
